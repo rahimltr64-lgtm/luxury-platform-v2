615bdc6b71b1a734379156c5ae60ef023a262c5d
rahimltr64-lgtm <rahimltr64@gmail.com>
2026-06-01 20:40:35 +0100

    Add categories management page

---
+++ app/dashboard/categories/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function CategoriesPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold mb-6">إدارة التصنيفات</h1>
+      {/* Content here */}
+    </div>
+  );
+}
